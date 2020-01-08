911da828ded3e8a6588069cc7b3e5b0f4c927056
Ashley Jimenez <anoelj86@yahoo.com>
2020-01-08 14:52:00 -0700

    heroku push

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(bodyParser.json());
 // const agreement = require("./agreement");
 // app.use(routes);
 
-// if (process.env.NODE_ENV === "production") {
-//   console.log("I made to prod!!");
-// Serve any static files
-// app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === "production") {
+  console.log("I made to prod!!");
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 // Handle React routing, return all requests to React app
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "index.html"));
